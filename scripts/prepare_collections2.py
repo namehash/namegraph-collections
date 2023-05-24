f5f536e46556c1fc841b6c20fbfac2610cc87691
Krzysztof Wróbel <djstrong@gmail.com>
2023-05-24 16:44:14 +0200

    own namehash

--- scripts/prepare_collections2.py
+++ scripts/prepare_collections2.py
@@ -4,12 +4,31 @@ from argparse import ArgumentParser
 
 import jsonlines as jsonlines
 import numpy as np
-from ens.utils import normal_name_to_hash
+from ens.constants import EMPTY_SHA3_BYTES
+from ens.utils import Web3
+from hexbytes import HexBytes
 from tqdm import tqdm
 
 from prepare_members_names import Collection
 
 MIN_VALUE = 1e-8
+
+def label_to_hash(label: str) -> HexBytes:
+    if "." in label:
+        raise ValueError(f"Cannot generate hash for label {label!r} with a '.'")
+    return Web3().keccak(text=label)
+
+
+def normal_name_to_hash(name: str) -> HexBytes:
+    node = EMPTY_SHA3_BYTES
+    if name:
+        labels = name.split(".")
+        for label in reversed(labels):
+            labelhash = label_to_hash(label)
+            assert isinstance(labelhash, bytes)
+            assert isinstance(node, bytes)
+            node = Web3().keccak(node + labelhash)
+    return node
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Prepare collections for ElasticSearch.')
